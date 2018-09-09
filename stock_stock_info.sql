7591645d16e8760945d80f10c13b92c79bfd1c5a
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-09-10 07:52:17 +0800

    Add files via upload

---
+++ stock_stock_info.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: stock
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `stock_info`
+--
+
+DROP TABLE IF EXISTS `stock_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_info` (
+  `state_dt` varchar(45) NOT NULL,
+  `stock_code` varchar(45) NOT NULL,
+  `open` decimal(20,2) DEFAULT NULL,
+  `close` decimal(20,2) DEFAULT NULL,
+  `high` decimal(20,2) DEFAULT NULL,
+  `low` decimal(20,2) DEFAULT NULL,
+  `vol` int(20) DEFAULT NULL,
+  `amount` decimal(30,2) DEFAULT NULL,
+  `pre_close` decimal(20,2) DEFAULT NULL,
+  `amt_change` decimal(20,2) DEFAULT NULL,
+  `pct_change` decimal(20,2) DEFAULT NULL,
+  `big_order_cntro` decimal(20,2) DEFAULT NULL,
+  `big_order_delt` decimal(20,2) DEFAULT NULL,
+  PRIMARY KEY (`state_dt`,`stock_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-10  7:36:36
