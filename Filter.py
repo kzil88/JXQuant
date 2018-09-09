f7ccc10f56c6fd41667aa2859a7661b3bbcff5c0
Mingxin WANG <37770806+kzil88@users.noreply.github.com>
2018-09-10 07:38:28 +0800

    Update Filter.py

--- Filter.py
+++ Filter.py
@@ -16,7 +16,7 @@ def filter_main(stock_new,state_dt,predict_dt,poz):
     deal = Deal.Deal(state_dt)
     stock_pool_local = deal.stock_pool
     for stock in stock_pool_local:
-        sql_predict = "select predict from good_pool_all a where a.state_dt = '%s' and a.stock_code = '%s'"%(predict_dt,stock)
+        sql_predict = "select predict from model_ev_resu a where a.state_dt = '%s' and a.stock_code = '%s'"%(predict_dt,stock)
         cursor.execute(sql_predict)
         done_set_predict = cursor.fetchall()
         predict = 0
@@ -29,7 +29,7 @@ def filter_main(stock_new,state_dt,predict_dt,poz):
         deal_buy = Deal.Deal(state_dt)
 
         # # 如果模型f1分值低于50则不买入
-        # sql_f1_check = "select * from good_pool_all a where a.stock_code = '%s' and a.state_dt < '%s' order by a.state_dt desc limit 1"%(stock_new[stock_index],state_dt)
+        # sql_f1_check = "select * from model_ev_resu a where a.stock_code = '%s' and a.state_dt < '%s' order by a.state_dt desc limit 1"%(stock_new[stock_index],state_dt)
         # cursor.execute(sql_f1_check)
         # done_check = cursor.fetchall()
         # db.commit()
